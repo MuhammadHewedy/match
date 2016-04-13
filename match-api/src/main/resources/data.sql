c815de9c400c5626392e14fd7a42a8497385d676
mohammad <mohammed_a_hewedy@hotmail.com>
2016-04-13 23:34:12 +0300

    add comment

--- match-api/src/main/resources/data.sql
+++ match-api/src/main/resources/data.sql
@@ -1,3 +1,4 @@
+-- password 12345678 
 INSERT INTO user (`created_by`, `created_date`, `modified_by`, `modified_date`, `enabled`, `password`, `role`, `username`) 
   SELECT -1, NOW(), -1, NOW(), TRUE, '$2a$10$53dXuhNlKSbXJ6cvFls/jeG2gemezsdk85OHfWVEmmEVnGuMG7i5S', 'ROLE_SUPER_ADMIN', 'superadmin' FROM DUAL
 WHERE NOT EXISTS 
