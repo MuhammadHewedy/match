9ffd2f1978dcdcc409ad198333db2a20b4c6eba5
mohammad <mohammed_a_hewedy@hotmail.com>
2016-04-13 12:22:54 +0300

    reformat code

--- match-web/app/login/controller.js
+++ match-web/app/login/controller.js
@@ -3,10 +3,10 @@
 angular.module('myApp.login', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/login', {
-    templateUrl: 'login/view.html',
-    controller: 'LoginCtrl'
-  });
+    $routeProvider.when('/login', {
+        templateUrl: 'login/view.html',
+        controller: 'LoginCtrl'
+    });
 }])
 
 .controller('LoginCtrl', [function() {
