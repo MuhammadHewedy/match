b83b3bcd6039d1baa475bbf383d3b0a72da04c21
mohammad <mohammed_a_hewedy@hotmail.com>
2016-04-14 12:07:15 +0300

    working on adding applicant

--- match-web/app/app.js
+++ match-web/app/app.js
@@ -2,7 +2,7 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-    'ngRoute', 'pascalprecht.translate', 'ngResource'
+    'ngRoute', 'pascalprecht.translate', 'ngResource', 'ngMessages'
 ]).
 config(['$routeProvider', '$httpProvider', '$translateProvider', function($routeProvider, $httpProvider, $translateProvider) {
 
