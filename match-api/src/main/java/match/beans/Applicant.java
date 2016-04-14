cc97ca37de9b919e45fe34f3259412bf7c4270ca
mohammad <mohammed_a_hewedy@hotmail.com>
2016-04-14 11:41:28 +0300

    add applicant fields

--- match-api/src/main/java/match/beans/Applicant.java
+++ match-api/src/main/java/match/beans/Applicant.java
@@ -15,6 +15,34 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = false)
 public class Applicant extends BaseEntity implements Serializable {
 	private static final long serialVersionUID = -7696991411016302499L;
+	
+	private String gender;
+	private Short age;
+	private String nationality;
+	private String countryOfPlace;
+	private String governorate;
+	private String bodyShape;
+	private Double weight;
+	private Double lenght;
+	private String eyeColor;
+	private String hairColor;
+	private String skinColor;
+	private String secondaryLang;
+	private String educationLevel;
+	private String job;
+	private String religion;
+	private String maritalStatus;
+	private Boolean haveKids;
+	private Boolean likeToHaveKids;
+	private Double monthlyIncome;
+	private String religionLevel;
+	private Boolean hijab;
+	private String socialLevel;
+	private String about;
+	private String partenerCharcs;
+	private String phoneNumber;
+	private String fatherPhoneNumber;
+	private String idImagePath;
 
 	@NotNull
 	@OneToOne
