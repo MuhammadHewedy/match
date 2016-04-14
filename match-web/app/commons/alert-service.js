f983d29a547c9d5a45eb1459253f99e37b359289
mohammad <mohammed_a_hewedy@hotmail.com>
2016-04-14 17:42:55 +0300

    autoHide

--- match-web/app/commons/alert-service.js
+++ match-web/app/commons/alert-service.js
@@ -5,17 +5,17 @@ angular.module('myApp')
 
 .factory('AlertService', ['$rootScope', function($rootScope) {
     return {
-        success: function(msg, link) {
+        success: function(msg, link, timeMillis) {
             var alert = {
                 message: msg ? msg : 'success',
                 type: 'success',
                 link: link
             }
-
             $rootScope.alerts = [];
             $rootScope.alerts.push(alert);
+            autoHide(timeMillis ? timeMillis : 500)
         },
-        error: function(msg, link) {
+        error: function(msg, link, timeMillis) {
             var alert = {
                 message: msg ? msg : 'fail',
                 type: 'danger',
@@ -23,6 +23,13 @@ angular.module('myApp')
             }
             $rootScope.alerts = [];
             $rootScope.alerts.push(alert);
+            autoHide(timeMillis ? timeMillis : 5000)
         }
     }
 }]);
+
+function autoHide(timeMillis) {
+    setTimeout(function() {
+        $('#alert').fadeOut('fast');
+    }, timeMillis);
+}
