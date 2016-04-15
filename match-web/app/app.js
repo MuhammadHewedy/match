fa03c31b0c82cf3e707fd7d181a0ac9086b88ee1
mohammad <mohammed_a_hewedy@hotmail.com>
2016-04-15 11:21:39 +0300

    add bootstrap + ng switch

--- match-web/app/app.js
+++ match-web/app/app.js
@@ -2,7 +2,7 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-    'ngRoute', 'ui.bootstrap', 'pascalprecht.translate', 'ngResource', 'ngMessages'
+    'ngRoute', 'ui.bootstrap', 'pascalprecht.translate', 'ngResource', 'ngMessages', 'frapontillo.bootstrap-switch'
 ]).
 config(['$routeProvider', '$httpProvider', '$translateProvider', function($routeProvider, $httpProvider, $translateProvider) {
 
