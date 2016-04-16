fd8543e7cad0017dd9980c96d3d8457756ff1a99
mohammad <mohammed_a_hewedy@hotmail.com>
2016-04-16 20:11:31 +0300

    app properties fixation

--- heroku-deploy.sh
+++ heroku-deploy.sh
@@ -2,8 +2,7 @@ TARGET=../match-deploy
 mkdir $TARGET
 cp -rf match-api/ $TARGET/
 cp -rf match-web/app/ $TARGET/src/main/resources/static/
-mv -f $TARGET/src/main/resources/application-heroku.properties $TARGET/src/main/resources/application.properties
 cat >$TARGET/Procfile <<EOF
-web: java -Dserver.port=\$PORT -jar target/*.jar
+web: java -Dspring.profiles.active=heroku -Dserver.port=\$PORT -jar target/*.jar
 EOF
 chmod +x $TARGET/Procfile
