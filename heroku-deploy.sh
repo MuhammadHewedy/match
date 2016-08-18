336f7ae145344db4a6155ae7fdff67f887e38cde
Muhammad Hewedy <mhewedy@gmail.com>
2016-08-19 00:18:06 +0300

    no need for this stupid step, since web apps can be packed inside the main spring boot app as jar dependencies

--- heroku-deploy.sh
+++
@@ -1,8 +0,0 @@
-TARGET=../match-deploy
-mkdir $TARGET
-cp -rf match-api/ $TARGET/
-cp -rf match-web/app/ $TARGET/src/main/resources/static/
-cat >$TARGET/Procfile <<EOF
-web: java -Dspring.profiles.active=heroku -Dserver.port=\$PORT -jar target/*.jar
-EOF
-chmod +x $TARGET/Procfile
