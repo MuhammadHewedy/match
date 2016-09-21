b6ac80659bbddb5a9903dfef9d4f1360a5d923f0
Muhammad Hewedy <mhewedy@gmail.com>
2016-09-21 09:26:01 +0300

    assert the password never return from the server as part of the user object

--- match-api/src/test/java/match/controllers/AdminControllerIntTest.java
+++ match-api/src/test/java/match/controllers/AdminControllerIntTest.java
@@ -24,18 +24,21 @@ import match.beans.User;
 import match.beans.repos.UserRepo;
 import match.util.TestUtil;
 
+@Transactional
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {match.Application.class})
 public class AdminControllerIntTest {
 
 	@Autowired
 	private UserRepo userRepo;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	
 	@Mock
 	private UserRepo mockedUserRepo;
 
 	@Mock
-	private PasswordEncoder passwordEncoder;
+	private PasswordEncoder mockedPasswordEncoder;
 
 	private MockMvc mockMvc;
 	
@@ -45,8 +48,8 @@ public class AdminControllerIntTest {
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
 
-		doReturn("encoded_password").when(passwordEncoder).encode(anyString());
-		this.mockedMockMvc = MockMvcBuilders.standaloneSetup(new AdminController(mockedUserRepo, passwordEncoder))
+		doReturn("encoded_password").when(mockedPasswordEncoder).encode(anyString());
+		this.mockedMockMvc = MockMvcBuilders.standaloneSetup(new AdminController(mockedUserRepo, mockedPasswordEncoder))
 				.build();
 		this.mockMvc = MockMvcBuilders.standaloneSetup(new AdminController(userRepo, passwordEncoder)).build();
 	}
@@ -91,11 +94,10 @@ public class AdminControllerIntTest {
 		.andExpect(status().isOk());
 	}
 	
-	@Transactional
 	@Test
 	public void testSaveDupValidUser() throws Exception {
 		User user = new User();
-		user.setUsername("admin");
+		user.setUsername("testSaveDupValidUser");
 		user.setPassword("123456");
 
 		this.mockMvc.perform(post("/api/admins")
@@ -111,4 +113,26 @@ public class AdminControllerIntTest {
 		.andExpect(status().isBadRequest())
 		.andExpect(jsonPath("$.message").value("duplicate.user.name"));
 	}
+	
+	@Test
+	public void testGetUserDoesntReturnThePassword() throws Exception {
+		User user = new User();
+		user.setUsername("testGetUserDoesntReturnThePassword");
+		user.setPassword("123456");
+
+		this.mockMvc.perform(post("/api/admins")
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.content(TestUtil.toJson(user)))
+		.andDo(print())
+		.andExpect(status().isOk());
+		
+		this.mockMvc.perform(get("/api/admins/3")
+				.contentType(MediaType.APPLICATION_JSON_UTF8))
+		.andDo(print())
+		.andExpect(jsonPath("$.password").doesNotExist());
+	}
+	
+	// TODO test user object sent with a different role, test it saved with admin role regardless of the role it sent with
+	
+	
 }
